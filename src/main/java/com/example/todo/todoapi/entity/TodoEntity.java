60ae4f9d1ef31033b8069a7d21692a8047c5b961
hyeokinen <kkunh97@naver.com>
2023-01-18 14:35:29 +0900

    # todo entity 생성

---
+++ src/main/java/com/example/todo/todoapi/entity/TodoEntity.java
@@ -0,0 +1,36 @@
+package com.example.todo.todoapi.entity;
+
+// 일정관리 프로그램
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Setter @Getter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "todoId")
+@Builder
+
+@Entity
+@Table(name = "tbl_todo")
+
+public class TodoEntity {
+
+    // pk가 랜덤 문자열로 지정된다. 즉 절대 중복이 안나오는 방식중에 하나이다.
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String todoId;
+
+    @Column(nullable = false, length = 30)
+    private String title; // 제목
+    private boolean done; // 일정 완료 여부
+
+    @CreationTimestamp
+    private LocalDateTime createDate; // 등록시간
+
+}
